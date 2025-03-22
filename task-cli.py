c2f4fc27f37f4743ce0cb9dccdb36f506fe3fd8a
jwong2-godaddy <jwong2@godaddy.com>
2025-03-22 15:05:05 -0500

    - Add functionalities

---
+++ task-cli.py
@@ -0,0 +1,65 @@
+import cmd
+import shlex
+import tasks 
+
+valid_status = ["TODO", "IN-PROGRESS", "DONE"]
+
+class TaskCLI(cmd.Cmd):
+    intro = """Welcome to the task tracker, I am glad to help you with them.
+    
+    You can type help to check the valid commands.
+    """
+    
+    def do_greet(self, arg):
+        print("Hello, I am here to help with your keep track of your tasks")
+
+    def do_EOF(self, arg):        
+        return True
+    
+    def do_help(self, arg):        
+        print("""
+            - Add new tasks: add <decription>
+            - List all tasks: list
+            - List by status: list <DONE|TODO|IN-PROGRESS>
+            - Update task description: update <id> <description>
+            - Delete task: delete <id>
+            - Mark task IN-PROGRESS:  mark_in_progress <id>
+            - Mark task DONE:  mark_done <id>
+""")
+    
+    def do_quit(self, arg):        
+        print('Exiting the console')
+        return True
+    
+    def do_exit(self, arg):        
+        print('Exiting the console')
+        return True
+    
+    def do_add(self, description):
+        tasks.add_task(description=description.strip('\"'))
+
+    def do_list(self, status):
+        print(tasks.list_tasks(status))    
+
+    def do_delete(self, id):
+        tasks.delete_task(id)
+
+    def do_update(self, args):
+        args = shlex.split(args)
+        
+        if len(args) < 2:
+            print("Please type the correct arguments to update : update <id> <description>")
+        else:
+            task_id = int(args[0])
+            description = args[1]            
+            
+            tasks.update_task(task_id, description=description)            
+
+    def do_mark_in_progress(seld, id):
+        tasks.update_task(int(id), status="IN-PROGRESS")
+
+    def do_mark_done(seld, id):
+        tasks.update_task(int(id), status="DONE")
+
+if __name__ == '__main__':
+    TaskCLI().cmdloop()
---
+++ tasks.py
@@ -0,0 +1,114 @@
+import json
+import datetime
+from collections import defaultdict
+from collections import namedtuple
+from json import JSONEncoder
+
+
+DATA_FILE = "task_list.json"
+DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+class Task:
+
+    def __init__(self, id, description="", status="TODO", createdAt=datetime.datetime.now(), updatedAt=datetime.datetime.now()):
+        
+        self.id = id
+        self.description = description
+        self.status = status
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt	
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'description': self.description,
+            'status': self.status,
+            'createdAt': self.createdAt.strftime(DATE_FORMAT),
+            'updatedAt': self.updatedAt.strftime(DATE_FORMAT)
+
+        }
+    
+    @classmethod
+    def from_dict(cls, task_dict):
+        createdAtObj = datetime.datetime.strptime(task_dict['createdAt'], DATE_FORMAT)
+        updatedAtObj = datetime.datetime.strptime(task_dict['updatedAt'], DATE_FORMAT)
+        return cls(task_dict['id'], task_dict['description'], task_dict['status'], createdAtObj, updatedAtObj)
+
+    def __str__(self):
+        return f"id: {self.id} description: {self.description} status: {self.status} created: {self.createdAt} updated: {self.updatedAt}"
+
+
+def load_tasks():
+    try:
+        with open(DATA_FILE, 'r') as data_file:
+            tasks = json.load(data_file)
+            return [Task.from_dict(task) for task in tasks]
+    except FileNotFoundError:
+        print(f"File {DATA_FILE} not found")
+        return []
+        
+def save_task(task_list):
+    task_dict = [task.to_dict() for task in task_list]
+    with open(DATA_FILE, 'w') as data_file:
+        json.dump(task_dict, data_file, indent=2)
+
+def add_task(description, status='TODO'):
+    task_list = load_tasks()
+    id = next_id([task.to_dict() for task in task_list])
+    task_list.append(Task(id, description=description, status=status))
+    save_task(task_list)
+    print(f"Task {description} added successfully (ID={id})")
+
+def next_id(list_dict):
+    res = defaultdict(int)
+    for task in list_dict :
+        res['id'] = max(res['id'], task['id'])
+
+    return int(res['id'])+1
+
+def list_tasks(status):
+    task_list = load_tasks()
+    if bool(status and not status.isspace()):        
+        return [ task.to_dict() for task in task_list if task.status == status]
+    else: 
+        return [ task.to_dict() for task in task_list]
+    
+def delete_task(id):
+    task_list = [task for task in load_tasks() if task.id != int(id)]
+    save_task(task_list)
+
+def update_task(id, *args, **kwargs):
+    task_list = load_tasks()
+    index_list = [index for (index, item) in enumerate(task_list) if item.id == id]
+    if len(index_list) > 0 :    
+        index = index_list[0]
+        for key, val in kwargs.items():
+            if key == "description" :
+                task_list[index].description = val
+            if key == "status" :
+                task_list[index].status = val
+        
+        task_list[index].updatedAt = datetime.datetime.now()
+        
+        print([task.to_dict() for task in task_list])
+        save_task(task_list)
+    else :
+        print(f"Task with id {id} does not exist")
+
+# def show_data(data):
+#     df = pd.DataFrame.from_dict(data)
+#     print(df)
+
+
+if __name__ == '__main__':
+    task_list = load_tasks()
+    list_dict = [task.to_dict() for task in task_list]
+    res = defaultdict(int)
+    for task in list_dict :
+        print(task['id'])
+        res['id'] = max(res['id'], task['id'])
+
+    print(res['id']) 
+
+
+
